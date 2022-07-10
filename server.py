90280be27275614218e3847252fb1a7e87506093
kshramt <kshramt@example.com>
2022-07-10 13:45:49 +0900

    Use pathlib

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import datetime
 import gzip
 import json
 import os
+import pathlib
 import urllib
 
 import flask
@@ -11,35 +12,12 @@ class Err(Exception):
     pass
 
 
-def mkdir(path):
-    return os.makedirs(path, exist_ok=True)
-
-
-def jp(path, *more):
-    """
-    >>> jp(".", "a")
-    'a'
-    >>> jp("a", "b")
-    'a/b'
-    >>> jp("a", "b", "..")
-    'a'
-    >>> jp("a", "/b", "c")
-    'a/b/c'
-    >>> jp("gs://b", "c//d")
-    'gs://b/c/d'
-    """
-    puri = urllib.parse.urlparse(os.path.sep.join((path, os.path.sep.join(more))))
-    return urllib.parse.ParseResult(
-        **{**puri._asdict(), **dict(path=os.path.normpath(puri.path))}
-    ).geturl()
-
-
 DATA_DIR = os.environ.get("EBS_DATA_DIR", "data")
 DATA_CHECKPOINT_DIR = os.environ.get("EBS_DATA_CHECKPOINT_DIR", None)
 DATA_BASENAME = os.environ.get("EBS_DATA_BASENAME", "evidence_based_scheduling")
 
 app = flask.Flask(
-    __name__, static_folder=jp("build", "static"), template_folder="build"
+    __name__, static_folder=pathlib.Path("build", "static"), template_folder="build"
 )
 
 
@@ -53,7 +31,7 @@ def root():
 @app.route("/api/v1/get")
 def get():
     try:
-        with gzip.open(jp(DATA_DIR, DATA_BASENAME) + ".json.gz", "rt") as fp:
+        with gzip.open(pathlib.Path(DATA_DIR, DATA_BASENAME + ".json.gz"), "rt") as fp:
             data = json.load(fp)
     except IOError:
         data = None
@@ -71,13 +49,14 @@ def save(data):
     s = json.dumps(data, ensure_ascii=False, indent=2, sort_keys=True) + "\n"
     time = datetime.datetime.now().isoformat()
     if DATA_CHECKPOINT_DIR is not None:
-        mkdir(DATA_CHECKPOINT_DIR)
+        pathlib.Path(DATA_CHECKPOINT_DIR).mkdir(exist_ok=True, parents=True)
         with gzip.open(
-            jp(DATA_CHECKPOINT_DIR, DATA_BASENAME) + "_" + time + ".json.gz", "wt"
+            pathlib.Path(DATA_CHECKPOINT_DIR, DATA_BASENAME + "_" + time + ".json.gz"),
+            "wt",
         ) as fp:
             fp.write(s)
-    mkdir(DATA_DIR)
-    with gzip.open(jp(DATA_DIR, DATA_BASENAME) + ".json.gz", "wt") as fp:
+    pathlib.Path(DATA_DIR).mkdir(exist_ok=True, parents=True)
+    with gzip.open(pathlib.Path(DATA_DIR, DATA_BASENAME + ".json.gz"), "wt") as fp:
         fp.write(s)
     return time
 
