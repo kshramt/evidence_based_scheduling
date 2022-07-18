c674674aab8b4cbf1d3b04ee5815a46844aad65d
kshramt <kshramt@example.com>
2022-07-19 08:07:18 +0900

    nullable=False

--- api/api/models.py
+++ api/api/models.py
@@ -25,9 +25,10 @@ class Snapshot(Base):
             onupdate="restrict",
         ),
         index=True,
+        nullable=False,
     )
-    version_id = Column(Integer, index=True)
-    data = Column(String)
+    version_id = Column(Integer, index=True, nullable=False)
+    data = Column(String, nullable=False)
 
     # __table_args__ = (Index("index_user_id_version_id", "user_id", "version_id"),)
 
@@ -44,6 +45,7 @@ class Session(Base):
             onupdate="restrict",
         ),
         index=True,
+        nullable=False,
     )
 
     user = sqlalchemy.orm.relationship("User", back_populates="sessions")
@@ -53,6 +55,11 @@ class Patches(Base):
     __tablename__ = "patchess"
 
     id = Column(Integer, primary_key=True, index=True)
-    session_id = Column(Integer, index=True)
-    version_id = Column(Integer, index=True)
-    patches = Column(String)
+    session_id = Column(
+        Integer,
+        ForeignKey("sessions.id", ondelete="restrict", onupdate="restrict"),
+        index=True,
+        nullable=False,
+    )
+    version_id = Column(Integer, index=True, nullable=False)
+    patches = Column(String, nullable=False)
