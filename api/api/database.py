5e0597f6d3f6256d0577275135b159d72135b09e
kshramt <kshramt@example.com>
2022-07-19 01:04:41 +0900

    Set `naming_convention`

--- api/api/database.py
+++ api/api/database.py
@@ -25,7 +25,17 @@ SessionLocal = sqlalchemy.orm.sessionmaker(
     expire_on_commit=False,
 )
 
-Base = sqlalchemy.orm.declarative_base()
+Base = sqlalchemy.orm.declarative_base(
+    metadata=sqlalchemy.MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+)
 
 
 @sqlalchemy.event.listens_for(engine.sync_engine, "connect")
