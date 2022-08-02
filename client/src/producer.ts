1d9f0984953c9e346d1991371615457f46977bfa
kshramt <kshramt@example.com>
2022-08-02 22:21:40 +0900

    Add a missing file

---
+++ client/src/producer.ts
@@ -0,0 +1,21 @@
+import * as fast_json_patch from "fast-json-patch";
+import * as immer from "immer";
+
+export type { Operation as TOperation } from "fast-json-patch";
+export { applyPatch as apply_patch } from "fast-json-patch";
+
+export function produce_with_patche<X>(
+  x: X,
+  fn: (
+    x: immer.Draft<X>,
+  ) =>
+    | void
+    | undefined
+    | (immer.Draft<X> extends undefined ? typeof immer.nothing : never)
+    | immer.Draft<X>,
+) {
+  const y = immer.produce(x, fn);
+  const patch = fast_json_patch.compare(x, y);
+  const reverse_patch = fast_json_patch.compare(y, x);
+  return { value: y, patch, reverse_patch };
+}
