682c90ea03e6096c079e4d1c44a4dc60129970ec
kshramt <kshramt@example.com>
2022-08-07 21:12:57 +0900

    prettify:

--- client/src/lib.tsx
+++ client/src/lib.tsx
@@ -1548,7 +1548,7 @@ const DetailsImpl = (props: { node_id: types.TNodeId }) => {
     <hr className="my-[0.5em] border-gray-300 dark:border-gray-600 bg-gray-300 dark:bg-gray-600" />
   );
   return (
-    <div className="pt-[0.5em] bg-gray-200 dark:bg-gray-700">
+    <div className="pt-[0.5em] bg-gray-200 dark:bg-gray-900">
       {hline}
       <div className="flex w-fit gap-x-[0.25em] items-baseline">
         {ParseTocButton_of(dispatch, props.node_id)}
