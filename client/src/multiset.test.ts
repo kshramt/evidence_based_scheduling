346492c4ca094c583cb6ad142f8ab2fb8ce71f72
kshramt <kshramt@example.com>
2022-08-13 02:42:32 +0900

    A bit more tests

---
+++ client/src/multiset.test.ts
@@ -0,0 +1,14 @@
+import { MultiSet } from "./multiset";
+
+it("sum", () => {
+  const ms = new MultiSet<number>();
+  expect(ms.has(0)).toBeFalsy();
+  ms.add(0);
+  expect(ms.has(0)).toBeTruthy();
+  ms.add(0);
+  expect(ms.has(0)).toBeTruthy();
+  ms.delete(0);
+  expect(ms.has(0)).toBeTruthy();
+  ms.delete(0);
+  expect(ms.has(0)).toBeFalsy();
+});
