7b2096bbd7e17d93d32b3ea89267b6225d7ce8f0
kshramt <kshramt@example.com>
2022-11-25 16:02:24 +0900

    Disable cache

--- api/api/static_files.py
+++ api/api/static_files.py
@@ -4,7 +4,7 @@ import fastapi.staticfiles
 class StaticFiles(fastapi.staticfiles.StaticFiles):
     async def get_response(self, *args, **kwargs):
         resp = await super().get_response(*args, **kwargs)
-        path = str(getattr(resp, "path", ""))
-        if not (path.endswith(".html") or path.endswith("/")):
-            resp.headers["cache-control"] = "max-age=7776000, immutable"
+        # path = str(getattr(resp, "path", ""))
+        # if not (path.endswith(".html") or path.endswith("/")):
+        #     resp.headers["cache-control"] = "max-age=7776000, immutable"
         return resp
