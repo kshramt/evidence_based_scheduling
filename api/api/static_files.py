fe6d987dce647ef77bd985b3f53e7cc73b2c88d0
kshramt <kshramt@example.com>
2022-11-25 11:55:02 +0900

    Tweak cache-control

--- api/api/static_files.py
+++ api/api/static_files.py
@@ -4,6 +4,7 @@ import fastapi.staticfiles
 class StaticFiles(fastapi.staticfiles.StaticFiles):
     async def get_response(self, *args, **kwargs):
         resp = await super().get_response(*args, **kwargs)
-        if not str(getattr(resp, "path", "")).endswith(".html"):
+        path = str(getattr(resp, "path", ""))
+        if not (path.endswith(".html") or path.endswith("/")):
             resp.headers["cache-control"] = "max-age=7776000, immutable"
         return resp
