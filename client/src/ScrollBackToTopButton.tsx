f5bbeeedb1cb7ace54c0219a4871ae30601fb3d6
kshramt <kshramt@example.com>
2023-01-02 15:45:34 +0900

    prettify:

--- client/src/ScrollBackToTopButton.tsx
+++ client/src/ScrollBackToTopButton.tsx
@@ -3,15 +3,7 @@ import React from "react";
 const Default = (props: { className?: string; children?: React.ReactNode }) => {
   const ref = React.useRef<HTMLButtonElement>(null);
   const onClick = React.useCallback(() => {
-    const el = ref.current;
-    if (el === null) {
-      return;
-    }
-    const pel = el.parentElement;
-    if (pel === null) {
-      return;
-    }
-    pel.scroll({ top: 0 });
+    ref.current?.parentElement?.scroll({ top: 0 });
   }, []);
   return (
     <button onClick={onClick} ref={ref} className={props.className}>
