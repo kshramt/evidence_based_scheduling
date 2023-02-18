3f236346c780afe6bd0b096d88233cc4fa9691b8
kshramt <kshramt@example.com>
2023-02-19 01:14:59 +0900

    Parameterize URL

--- db/scripts/migrate.sh
+++ db/scripts/migrate.sh
@@ -18,4 +18,4 @@ if [[ ${ENV:-prod} != dev ]]; then
     sed -i'' -e 's/@APP_USER_PASSWORD@/'"${_POSTGRES_APP_USER_PASSWORD:?}"'/g' "db/migrations/00000000000000_create_app_user.sql"
 fi
 
-dbmate --wait --url "postgres://postgres@/postgres" migrate --verbose
+dbmate --wait --url "${URL:-postgres://postgres@/postgres}" migrate --verbose
