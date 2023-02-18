d8bff6c1d1e7d96ebcedac52eb23cfff6c40ffba
kshramt <kshramt@example.com>
2023-02-18 19:03:31 +0900

    debug:

--- bake.py
+++ bake.py
@@ -32,11 +32,11 @@ def run(args):
     ):
         ks = []
         for target, image_name in (
-            ("test_api", "/test/api"),
-            ("test_client", "/test/client"),
-            ("prod_api", "/api"),
+            # ("test_api", "/test/api"),
+            # ("test_client", "/test/client"),
+            # ("prod_api", "/api"),
             ("prod_envoy", "/envoy"),
-            ("prod_nginx", "/nginx"),
+            # ("prod_nginx", "/nginx"),
         ):
             k = f"{target}-{platform.os}-{platform.arch}"
             v = {
--- scripts/build_test_push.sh
+++ scripts/build_test_push.sh
@@ -30,11 +30,7 @@ readonly host_os
 python3 bake.py --sha="${github_sha}" --ref_b64="${ref_b64}" | tee bake.json
 docker buildx bake --file bake.json "${os}-${arch}"
 
-if [[ "${os}" = "${host_os}" && "${arch}" = "${host_arch}" ]]; then
-  # docker run ghcr.io/kshramt/evidence_based_scheduling/test/api:"${github_sha}-${os}-${arch}" scripts/check.sh
-  # docker run ghcr.io/kshramt/evidence_based_scheduling/test/client:"${github_sha}-${os}-${arch}" scripts/check.sh
-
-for service in api nginx envoy
+for service in envoy
 do
   img="ghcr.io/kshramt/evidence_based_scheduling/${service}"
   docker push "${img}":"${github_sha}-${os}-${arch}"
@@ -48,15 +44,14 @@ done
 
   mkdir -p ./my_data
   {
-     echo DOCKER_API_DATA_DIR="./my_data"
-     echo USE_LITESTREAM="no"
-     echo REPLICA_URI=""
-     echo DOCKER_API_VERSION="${github_sha}-${os}-${arch}"
-     echo DOCKER_NGINX_VERSION="${github_sha}-${os}-${arch}"
-     echo DOCKER_ENVOY_VERSION="${github_sha}-${os}-${arch}"
+     echo _DOCKER_API_DATA_DIR="./my_data"
+     echo _USE_LITESTREAM="no"
+     echo _REPLICA_URI=""
+     echo _DOCKER_API_VERSION="${github_sha}-${os}-${arch}"
+     echo _DOCKER_NGINX_VERSION="${github_sha}-${os}-${arch}"
+     echo _DOCKER_ENVOY_VERSION="${github_sha}-${os}-${arch}"
   } > .env
   cat .env
-  sleep 1
   docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up
 
   i=0
@@ -70,7 +65,6 @@ done
   if [[ ${i} -ge 10 ]]; then
       exit 1
   fi
-fi
 
 # for service in api nginx envoy
 # do
