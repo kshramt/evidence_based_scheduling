b17b4c06753807b8821d455c2b84ca0f7a122030
kshramt <kshramt@example.com>
2023-02-28 20:30:02 +0800

    `networkTimeoutSeconds` should be set

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -23,7 +23,10 @@ clientsClaim();
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST);
 
-const network_first = new NetworkFirst({cacheName: "app"});
+const network_first = new NetworkFirst({
+  cacheName: "app",
+  networkTimeoutSeconds: 5,
+});
 registerRoute("/", network_first);
 registerRoute(/^\/app\/.+/, network_first);
 
