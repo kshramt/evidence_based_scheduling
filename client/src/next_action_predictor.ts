b673ec34d83da6475b7e2930296dac06f60f63dc
kshramt <kshramt@example.com>
2023-05-05 11:22:25 +0900

    Use the in-source testing capability to avoid unnecessary `export`s

--- client/src/next_action_predictor.ts
+++ client/src/next_action_predictor.ts
@@ -109,3 +109,59 @@ export class BiGramPredictor<K> {
   };
   default_count_of = () => 0;
 }
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  it("TriGramPredictor(1)", () => {
+    const predictor = new TriGramPredictor(1);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(predictor.fit(2).predict()).toEqual([]);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(predictor.fit(2).predict()).toEqual([0]);
+    expect(predictor.fit(3).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(new Set(predictor.fit(2).predict())).toEqual(new Set([0, 3]));
+  });
+
+  it("TriGramPredictor(0.9)", () => {
+    const predictor = new TriGramPredictor(0.9);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(predictor.fit(2).predict()).toEqual([]);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(predictor.fit(2).predict()).toEqual([0]);
+    expect(predictor.fit(3).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(predictor.fit(2).predict()).toEqual([3, 0]);
+  });
+
+  it("BiGramPredictor(1)", () => {
+    const predictor = new BiGramPredictor(1);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(predictor.fit(2).predict()).toEqual([]);
+    expect(predictor.fit(0).predict()).toEqual([1]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(predictor.fit(2).predict()).toEqual([0]);
+    expect(predictor.fit(3).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(new Set(predictor.fit(2).predict())).toEqual(new Set([0, 3]));
+  });
+
+  it("BiGramPredictor(0.9)", () => {
+    const predictor = new BiGramPredictor(0.9);
+    expect(predictor.fit(0).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([]);
+    expect(predictor.fit(2).predict()).toEqual([]);
+    expect(predictor.fit(0).predict()).toEqual([1]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(predictor.fit(2).predict()).toEqual([0]);
+    expect(predictor.fit(3).predict()).toEqual([]);
+    expect(predictor.fit(1).predict()).toEqual([2]);
+    expect(predictor.fit(2).predict()).toEqual([3, 0]);
+  });
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -62,4 +63,10 @@ export default defineConfig({
     },
   },
   base: "./",
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  test: {
+    includeSource: ["src/**/*.{js,ts,jsx,tsx}"],
+  },
 });
