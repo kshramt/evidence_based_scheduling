c1c0e7dfba554f2f0f936a2d5c0eed75c8321201
kshramt <kshramt@example.com>
2023-05-20 12:33:48 +0900

    Push in parallel

--- scripts/publish_manifests.sh
+++ scripts/publish_manifests.sh
@@ -22,12 +22,13 @@ for service in nginx envoy postgres api_v1 postgres_migration
 do
   img="ghcr.io/kshramt/evidence_based_scheduling/${service}"
   docker manifest create "${img}:h-${github_sha}"{,-linux-{amd64,arm64}}
-  docker manifest push "${img}:h-${github_sha}"
+  docker manifest push "${img}:h-${github_sha}" &
 
   if [[ "${ref}" = "refs/heads/main" ]]; then
     docker manifest create "${img}:"{"b-${run_number}","h-${github_sha}"-linux-{amd64,arm64}}
-    docker manifest push "${img}:b-${run_number}"
+    docker manifest push "${img}:b-${run_number}" &
     docker manifest create "${img}:"{latest,h-"${github_sha}"-linux-{amd64,arm64}}
-    docker manifest push "${img}:latest"
+    docker manifest push "${img}:latest" &
   fi
 done
+wait
