f067d8911d8145c67a0383186c8d3cf076cd196d
kshramt <kshramt@example.com>
2023-09-19 00:29:38 +0900

    Add a missing file

---
+++ client/src/Header/MenuButton/ExportStateButton/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import * as types from "src/types";
+import * as utils from "src/utils";
+
+const ExportStateButton = React.memo(() => {
+  const dispatch = types.useDispatch();
+  const handleClick = React.useCallback(() => {
+    dispatch((disptch, getState) => {
+      const state = getState();
+      utils.downloadJson("evidence_based_scheduling.json", state.data);
+    });
+  }, [dispatch]);
+  return (
+    <span onClick={handleClick} onDoubleClick={utils.prevent_propagation}>
+      Export State
+    </span>
+  );
+});
+
+export default ExportStateButton;
