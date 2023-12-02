c456f0fe9aaa4acb020c93d149e31316b3ecdeb4
kshramt <kshramt@example.com>
2023-12-02 15:34:09 +0000

    `npx prettier --write src/`

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -27,7 +27,9 @@ export class Auth {
 
   constructor() {
     this.#on_change_hooks = new Set();
-    this.#client = createClient<v2.paths>({ baseUrl: `${window.location.origin}/api/v2` });
+    this.#client = createClient<v2.paths>({
+      baseUrl: `${window.location.origin}/api/v2`,
+    });
     this.id_token = null;
     this.loading = db
       .then((db_) => {
