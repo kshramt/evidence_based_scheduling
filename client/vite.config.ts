dc27a085d70a3a06faaa2c54aed5437eb3d0dd8f
kshramt <kshramt@users.noreply.github.com>
2024-07-27 04:48:59 +0000

    chore: Update npm dependencies for React and React DOM versions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
+
+const ReactCompilerConfig = {}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +12,13 @@ export default defineConfig({
     target: "esnext",
   },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ]
+      }
+    }),
     VitePWA({
       // registerType: "autoUpdate",
       strategies: "injectManifest",
