f2b2814f3eb7223fe37d4474f008a3c389d53d97
kshramt <kshramt@users.noreply.github.com>
2026-01-02 01:50:26 +0900

    Format shell scripts for linting

--- db/scripts/migrate.sh
+++ db/scripts/migrate.sh
@@ -16,17 +16,16 @@ pushd "${dir}/.."
 
 sed -i'' -e 's/@APP_USER_PASSWORD@/'"${_POSTGRES_APP_USER_PASSWORD:?}"'/g' "db/migrations/00000000000000_create_app_user.sql"
 
-run(){
-   dbmate --wait --url "${URL:?}" migrate --verbose
+run() {
+	dbmate --wait --url "${URL:?}" migrate --verbose
 }
 
 if [[ -z "${CI:-}" ]]; then
-   run
+	run
 else
-   for _ in {1..2}
-   do
-      # shellcheck disable=SC2310
-      run && break;
-      sleep 5
-   done
+	for _ in {1..2}; do
+		# shellcheck disable=SC2310
+		run && break
+		sleep 5
+	done
 fi
